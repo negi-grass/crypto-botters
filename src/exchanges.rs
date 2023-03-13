f11a3a816bf024deb0af651befaf1980e76ab8f4
NegiGrass <121017049+negi-grass@users.noreply.github.com>
2023-03-13 16:50:28 +0900

    Copied files for each exchanges into src/exchanges

---
+++ src/exchanges.rs
@@ -0,0 +1,8 @@
+#[cfg(feature = "binance")]
+pub mod binance;
+#[cfg(feature = "bitflyer")]
+pub mod bitflyer;
+#[cfg(feature = "bybit")]
+pub mod bybit;
+#[cfg(feature = "coincheck")]
+pub mod coincheck;
